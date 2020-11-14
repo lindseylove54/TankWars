f56a32086ab6037b6f28221b54ff2a642ee48115
lindseyl@eng.utah.edu <lindseyl@eng.utah.edu>
2020-11-13 23:25:13 -0700

    Added labels, textboxes, and connect button to the View. Started on the handler for the button

--- TankWars/View/Form1.Designer.cs
+++ TankWars/View/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Server Address";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 24);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(402, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label serverAddressLabel;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.TextBox serverAddressBox;
     }
 }
 
--- TankWars/View/Form1.cs
+++ TankWars/View/Form1.cs
@@ -1,20 +1,67 @@
-﻿using System;
+﻿using NetworkUtil;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace View
 {
+    /**
+     * REQUIREMENTS FOR THE VIEW:
+     *      Allow a player to declare a name and choose a server by IP address or host name.
+     *      
+     *      Provide a way to display connection errors and to retry connecting.
+     *      
+     *      Draw the scene, including the tanks, projectiles, etc...The first 8 players(at least) should be drawn with a unique color or graphic 
+     *      that identifies that player. Beyond 8 players, you can reuse existing graphics or colors. The name, health, and score of each player should be displayed by their tank.
+     *      
+     *      Full credit will include polish and attention to detail, such as changing the color of the HP bar as it gets low, or an artistic way of drawing the beam attack and explosions.
+     *      
+     *      Your GUI should be able to keep up with the server and draw frames as fast as the server sends new information about the world, at least 60 frames per second.
+     *      
+     *      Follow the defined communication sequence and protocol (see below).
+     *      
+     *      Register keyboard and mouse handlers that recognize basic user inputs and invoke appropriate controller methods to process the inputs.
+     * **/
+
+    //
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            if (nameBox.Text.Equals("") || serverAddressBox.Text.Equals(""))
+            {
+                MessageBox.Show("Must fill out both Player Name and Server Address", "Error!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            //if server address contains a semicolon, we know its an ip 
+            else if (serverAddressBox.Text.Contains(":"))
+            {
+                string ip = serverAddressBox.Text.Substring(0, serverAddressBox.Text.IndexOf(':'));
+                int port = int.Parse(serverAddressBox.Text.Substring(serverAddressBox.Text.IndexOf(':') + 1));
+                //next create socket connection
+                //Socket connection = Networking.ConnectToServer.....
+            }
+
+            //if just host name, create socket connection with that
+            else
+            {
+                //Socket connection = Networking.ConnectToServer.....
+            }
+        }
+
+
     }
 }
